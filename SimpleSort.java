8647298ed345aaf5747145ce705386ad4b3fef50
Ethan-Fisher45 <ethan.fisher@oneidaihla.org>
2022-12-15 11:18:55 -0700

    Update SimpleSort.java
    Added Notes

--- SimpleSort.java
+++ SimpleSort.java
@@ -1,3 +1,7 @@
+//Ethan Fisher 12/16/22
+//Simple Sort Project
+//For MR. Gross programming Class
+
 public class SimpleSort {
     static int[] swapTwoArrayElements(int[] arrayToSwap, int lowerIndex){
 
@@ -15,27 +19,27 @@ public class SimpleSort {
     public static void main(String[] args){
         int[] arrayToSort={1,3,4,5,1,23,57,126,4,543,345,23,12,45,67,97};// this creates the array.
         System.out.println("Unsorted Array");
-        for (int i=0; i<arrayToSort.length; i++){
+        for (int i=0; i<arrayToSort.length; i++){//printing out unsorted array
 
             System.out.println(arrayToSort[i]);
         }
-        Boolean haveswapped = true;
-        while(haveswapped){
+        Boolean haveswapped = true;//creating a flag
+        while(haveswapped){//checks the list until we have gone through without swapping anything
             haveswapped = false;
 
             
 
 
-            for (int i=0; i<arrayToSort.length-1; i++){
+            for (int i=0; i<arrayToSort.length-1; i++){// sorting the array
                 if (arrayToSort[i]>arrayToSort[i+1]){
                     arrayToSort = swapTwoArrayElements(arrayToSort, i);
-                    haveswapped = true;
+                    haveswapped = true;//this is our flag to know that we need to check again
                 }   
             }
 
         }
         System.out.println("sorted Array");
-        for (int i=0; i<arrayToSort.length; i++){
+        for (int i=0; i<arrayToSort.length; i++){//printing out sorted array
 
             System.out.println(arrayToSort[i]);
         }
